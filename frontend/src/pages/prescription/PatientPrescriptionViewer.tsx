33547bc2ca5d4c7889531b7bf9942b7a886d64f1
PRACHI MADANE <prachimadane2004@gmail.com>
2025-08-11 22:19:13 +0530

    Update PatientPrescriptionViewer.tsx

--- frontend/src/pages/prescription/PatientPrescriptionViewer.tsx
+++ frontend/src/pages/prescription/PatientPrescriptionViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+kr ye paste:import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import {
